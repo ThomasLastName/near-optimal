e31a0593b680e94e6361563346f60354e96e05db
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-10-23 21:14:30 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+### ~~~
+## ~~~ Originally from https://github.com/maet3608/minimal-setup-py/blob/master/setup.py
+### ~~~ 
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'near_optimal',
+    version = '0.0.0',
+    url = 'https://github.com/ThomasLastName/python-package-template',
+    author = 'Thomas Winckelman',
+    author_email = 'winckelman@tamu.edu',
+    description = 'Code for "optimal recovery for neural networks"',
+    packages = find_packages(),
+    install_requires = ["requests","cvxpy"])
