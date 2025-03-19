33153de0504968b26f4cf6666be929ef91f00b4f
Thomas (Tom) Winckelman <thomas.k.winckelman@gmail.com>
2025-03-19 18:43:31 -0500

    Toggled visual settings for presentation

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'near_optimal',
-    version = '0.7.0',
+    version = '0.7.1',
     url = 'https://github.com/ThomasLastName/near-optimal',
     author = 'Thomas Winckelman',
     author_email = 'winckelman@tamu.edu',
