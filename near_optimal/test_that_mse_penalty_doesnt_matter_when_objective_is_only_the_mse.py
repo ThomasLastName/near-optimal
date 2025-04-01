583aa35e77864e2c82ada89e09ba6d583af29f56
ThomasLastName <thomas.k.winckelman@gmail.com>
2025-04-01 09:11:19 -0500

    Add `weighted_mean` to tests

--- near_optimal/test_that_mse_penalty_doesnt_matter_when_objective_is_only_the_mse.py
+++ near_optimal/test_that_mse_penalty_doesnt_matter_when_objective_is_only_the_mse.py
@@ -14,6 +14,7 @@ from near_optimal.quadratic_univar import x_train, y_train, k, m, DualSpline
 v = DualSpline( x_train, y_train )
 
 
+WEIGHTED_MEAN = ( True, False )
 MSE_PENALTY = np.linspace( 0, 2*m, 31 )
 TOL = ( 1e-6, 1e-9 )
 BREAKPOINT_REG = ( 0, 1e-12, 1e-8, 1e-4 )
@@ -25,10 +26,10 @@ with support_for_progress_bars():
         print(f"    Testing mse_penalty={bcolors.HEADER + str(mse_penalty) + bcolors.OKGREEN} (will test up to {MSE_PENALTY.max()})")
         print("")
         lst = []
-        for tol, breakpoint_reg in tqdm( product(TOL,BREAKPOINT_REG), total=len(TOL)*len(BREAKPOINT_REG), desc="MSE Minimization", ascii=" >=" ):
+        for tol, breakpoint_reg, weighted_mean in tqdm( product(TOL,BREAKPOINT_REG,WEIGHTED_MEAN), total=len(TOL)*len(BREAKPOINT_REG), desc="MSE Minimization", ascii=" >=" ):
             #
             # ~~~ Solve the dual
-            dual_max = v.solve_dual_of_mse_minimization( mse_penalty=mse_penalty, solver=cvx.SCS, eps_abs=tol, eps_rel=tol, eps_infeas=tol/1000, breakpoint_reg=breakpoint_reg, print_info=False ).objective.value
+            dual_max = v.solve_dual_of_mse_minimization( mse_penalty=mse_penalty, solver=cvx.SCS, eps_abs=tol, eps_rel=tol, eps_infeas=tol/1000, breakpoint_reg=breakpoint_reg, weighted_mean=weighted_mean, print_info=False ).objective.value
             #
             # ~~~ Append the results as a new row in the DataFrame
             lst.append(dual_max)
