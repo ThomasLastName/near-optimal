8d7d0d46ada5eb59d106fe875bb6d0750b99369e
ThomasLastName <thomas.k.winckelman@gmail.com>
2026-01-20 15:49:36 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,21 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'near_optimal',
-    version = '0.9.12',
+    version = '0.9.13',
     url = 'https://github.com/ThomasLastName/near-optimal',
     author = 'Thomas Winckelman',
     author_email = 'winckelman@tamu.edu',
     description = 'Code for "optimal recovery for neural networks"',
     packages = find_packages(),
     install_requires = [
+            "numpy",
+            "scipy>=1.6.1",
+            "pandas",
             "cvxpy",
+            "SCS",
+            "matplotlib",
+            "seaborn",
+            "tqdm",
+            "torch",
             "quality_of_life @ git+https://github.com/ThomasLastName/quality-of-life.git"
         ])
