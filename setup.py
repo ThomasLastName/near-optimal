68db52e6343514ecd09e4710e4fbe412efe8e503
ThomasLastName <thomas.k.winckelman@gmail.com>
2026-03-06 18:18:31 -0600

    Publish

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'near_optimal',
-    version = '0.13.0',
+    version = '1.0.0',
     url = 'https://github.com/ThomasLastName/near-optimal',
     author = 'Thomas Winckelman',
     author_email = 'winckelman@tamu.edu',
